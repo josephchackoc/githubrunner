4cf67c5c4e025be349e24863d338b75ca7f47631
josephchackoc <josephchackoc@gmail.com>
2026-05-07 04:13:43 +0000

    Dockerfile for gh runner

---
+++ runners/docker/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+test -n "$GHPATTOKEN" || (echo "GHTOKEN must be set" && false)
+test -n "$GHREPO" || (echo "GHREPO must be set" && false)
+RUNNERLABEL=${RUNNERLABEL:-myrunner1}
+
+regToken=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GHPATTOKEN" \
+  -H "X-GitHub-Api-Version: 2026-03-10" \
+  https://api.github.com/repos/${GHREPO}/actions/runners/registration-token | jq -r '.token')
+
+$HOME/actions-runner/config.sh --unattended --url https://github.com/${GHREPO} --token $regToken --labels $RUNNERLABEL
+
+$HOME/actions-runner/run.sh
