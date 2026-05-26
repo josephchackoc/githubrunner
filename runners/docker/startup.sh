f662b86476af568aabbd764871a775bda4936a44
josephchackoc <josephchackoc@gmail.com>
2026-05-26 15:15:53 +0530

    Implement cleanup for GitHub Actions runner
    Added cleanup logic to deregister the runner after execution.

--- runners/docker/startup.sh
+++ runners/docker/startup.sh
@@ -14,3 +14,15 @@ regToken=$(curl -L \
 $HOME/actions-runner/config.sh --unattended --url https://github.com/${GHREPO} --token $regToken --labels $RUNNERLABEL
 
 $HOME/actions-runner/run.sh
+
+# Cleanup after runner exits
+if [ -e $HOME/actions-runner/config.sh ]; then
+  cd $HOME/actions-runner
+  deregToken=$(curl -L \
+              -X POST \
+              -H "Accept: application/vnd.github+json" \
+              -H "Authorization: Bearer $GHPATTOKEN" \
+              -H "X-GitHub-Api-Version: 2026-03-10" \
+              https://api.github.com/repos/${GHREPO}/actions/runners/registration-token | jq -r '.token')
+  ./config.sh remove --unattended --token "$deregToken"
+fi
