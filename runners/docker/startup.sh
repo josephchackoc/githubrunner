78b10b0086d005d7b22bd346d713775bf575c306
josephchackoc <josephchackoc@gmail.com>
2026-05-26 15:27:00 +0530

    Update startup.sh

--- runners/docker/startup.sh
+++ runners/docker/startup.sh
@@ -11,13 +11,14 @@ regToken=$(curl -L \
   -H "X-GitHub-Api-Version: 2026-03-10" \
   https://api.github.com/repos/${GHREPO}/actions/runners/registration-token | jq -r '.token')
 
-$HOME/actions-runner/config.sh --unattended --url https://github.com/${GHREPO} --token $regToken --labels $RUNNERLABEL
+cd  $HOME/actions-runner/
+./config.sh --unattended --url https://github.com/${GHREPO} --token $regToken --labels $RUNNERLABEL
 
-$HOME/actions-runner/run.sh
+./run.sh
 
 # Cleanup after runner exits
 if [ -e $HOME/actions-runner/config.sh ]; then
-  cd $HOME/actions-runner
+  
   deregToken=$(curl -L \
               -X POST \
               -H "Accept: application/vnd.github+json" \
